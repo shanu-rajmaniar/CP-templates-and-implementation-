e5ad56e3c1da14ad0cbb8bdd3d8fbd339435f002
Shanu-Rajmaniar <73025553+Shanu-Rajmaniar@users.noreply.github.com>
2022-01-17 14:11:41 +0530

    Create Disjoint Set Union.cpp

---
+++ Disjoint Set Union.cpp
@@ -0,0 +1,49 @@
+struct UnionFind {
+    int n, set_size, *parent, * rank;
+    UnionFind(){}
+    UnionFind(int a) {
+        n = set_size = a;
+        parent = new int[n + 1];
+        rank = new int[n + 1];
+        for(int i = 1;i <= n;i++){
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    int find(int x) {
+        if(x != parent[x]){
+            return parent[x] = find(parent[x]);
+        }
+        return x;
+    }
+
+    void merge(int x, int y) {
+        int xroot = find(x), yroot = find(y);
+        if(xroot != yroot){
+            if(rank[xroot] >= rank[yroot]) {
+                parent[yroot] = xroot;
+                rank[xroot] += rank[yroot];
+            }
+            else {
+                parent[xroot] = yroot;
+                rank[yroot] += rank[xroot];
+            }
+            set_size -= 1;
+        }
+    }
+
+    void reset() {
+        set_size = n;
+        for(int i = 1;i <= n;i++){
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    void print(){
+        for(int i = 1;i <= n;i++){
+            cout << i << " -> " << parent[i] << endl;
+        }
+    }
+};
