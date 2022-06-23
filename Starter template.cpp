fc3caf122075d572a8df8ec47ef0a75c931d6b33
Shanu-Rajmaniar <73025553+Shanu-Rajmaniar@users.noreply.github.com>
2022-06-23 14:19:44 +0530

    Updated

--- Starter template.cpp
+++ Starter template.cpp
@@ -10,31 +10,53 @@ using ld = long double;
 #define int long long
 #define all(x) (x).begin(), (x).end()
 
-template<typename T> // cin >> vector<T>
-istream& operator>> (istream &istream, vector<T> &v)
-{ for (auto &it : v) cin >> it; return istream; }
+template<typename T>
+istream& operator >>(istream& istream, vector<T>& v) {
+    for(auto &it : v) {
+        cin >> it;
+    }
+    return istream;
+}
 
-template<typename T> // cout << vector<T>
-ostream& operator<< (ostream &ostream, const vector<T> &c)
-{ for (auto &it : c) cout << it << " " ; return ostream; }
+template<typename T>
+ostream& operator <<(ostream& ostream, vector<T>& v) {
+    for(auto &it : v) {
+        cout << it << " ";
+    }
+    return ostream;
+}
 
-template<typename T> // print 2d vector<T>
-ostream& operator <<(ostream& ostream,const vector<vector<T>>& v) 
-{ for(auto& row : v) { for(auto& cell : row) { cout << cell << " "; } cout << endl; } return ostream; }
-
-ll power(ll a, ll b){
+ll power(ll a, ll b) {
     ll res = 1;
-    while (b > 0) { if (b & 1) res = res * a; a = a * a; b >>= 1;}
+    while (b > 0) {
+        if (b & 1) res = res * a; a = a * a; b >>= 1;
+    }
     return res;
 }
 
-int main(){
+ll binpow(ll a, ll b, ll m) {
+    a %= m; ll res = 1;
+    while (b > 0) {
+        if (b & 1) res = res * a % m; a = a * a % m; b >>= 1;
+    }
+    return res;
+}
+
+int64_t floor_div(int64_t a, int64_t b) {
+    return a / b - ((a ^ b) < 0 && a % b != 0);
+}
+
+int64_t ceil_div(int64_t a, int64_t b) {
+    return a / b + ((a ^ b) > 0 && a % b != 0);
+}
+
+int32_t main() {
 
     ios_base::sync_with_stdio(false);
     cin.tie(0);cout.tie(0);
 
-    ll _tc;cin >> _tc;
-    while(_tc--){
+    ll _tc; cin >> _tc;
+    while(_tc--) {
         
     }
     return 0;
