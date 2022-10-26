b6f9d9e0e867c64efa43400717878a20e8d13e76
Shanu Rajmaniar <73025553+Shanu-Rajmaniar@users.noreply.github.com>
2022-10-26 23:07:29 +0530

    Create ModularArithmetic.cpp

---
+++ ModularArithmetic.cpp
@@ -0,0 +1,45 @@
+const ll MOD = 998244353;
+template<int MOD> struct mint {
+	static const int mod = MOD;
+	int v; explicit operator int() const { return v; }
+	mint():v(0) {}
+	mint(ll _v) { v = int((-MOD < _v && _v < MOD) ? _v : _v % MOD);
+		if (v < 0) v += MOD; }
+	bool operator==(const mint& o) const {
+		return v == o.v; }
+	friend bool operator!=(const mint& a, const mint& b) { 
+		return !(a == b); }
+	friend bool operator<(const mint& a, const mint& b) { 
+		return a.v < b.v; }
+   
+	mint& operator+=(const mint& o) { 
+		if ((v += o.v) >= MOD) v -= MOD; 
+		return *this; }
+	mint& operator-=(const mint& o) { 
+		if ((v -= o.v) < 0) v += MOD; 
+		return *this; }
+	mint& operator*=(const mint& o) { 
+		v = int((ll)v*o.v%MOD); return *this; }
+	mint& operator/=(const mint& o) { return (*this) *= inv(o); }
+	friend mint pow(mint a, ll p) {
+		mint ans = 1; assert(p >= 0);
+		for (; p; p /= 2, a *= a) if (p&1) ans *= a;
+		return ans; }
+	friend mint inv(const mint& a) { assert(a.v != 0); 
+		return pow(a,MOD-2); }
+		
+	mint operator-() const { return mint(-v); }
+	mint& operator++() { return *this += 1; }
+	mint& operator--() { return *this -= 1; }
+	friend mint operator+(mint a, const mint& b) { return a += b; }
+	friend mint operator-(mint a, const mint& b) { return a -= b; }
+	friend mint operator*(mint a, const mint& b) { return a *= b; }
+	friend mint operator/(mint a, const mint& b) { return a /= b; }
+  friend ostream& operator<<(ostream& os, const mint& m) {
+      os << m.v; return os; }
+  friend istream& operator>>(istream& is, mint& m) {
+      ll x; is >> x;
+      m.v = x;
+      return is; }
+};
+using mi = mint<MOD>;
